071081a455ea93b3e435ef91ffc6e09542124a65
Alex Velasquez <jtinnesco@gmail.com>
2020-03-29 21:09:25 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,13 +16,16 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
+    if !Artist.find_by(:name => params["Name"])
+      @song = Song.create(:name => params["Name"])
+      @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+      @song.genre_ids = params[:genres]
+      @song.save
+      erb :"songs/show"
+    else
+      
 
-    @song = Song.create(:name => params["Name"])
-    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-    @song.genre_ids = params[:genres]
-    @song.save
 
-    erb :"songs/show"
   end
 
 
