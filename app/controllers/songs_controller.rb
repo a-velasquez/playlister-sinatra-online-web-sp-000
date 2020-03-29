b2d6b6cf1644b7b69b8be6580aa0bbddb6c28f43
Alex Velasquez <jtinnesco@gmail.com>
2020-03-29 20:49:31 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,31 +10,31 @@ class SongsController < ApplicationController
     erb :"songs/new"
   end
 
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
 
- post '/songs' do
 
-   artist_entry = params[:song][:artist]
-   if Artist.find_by(:name => artist_entry)
-     artist = Artist.find_by(:name => artist_entry)
-   else
-     @song = Song.create(:name => params[:song][:name])
-     artist = Artist.create(:name => artist_entry)
-   end
-   @song.artist = artist
-
-   genre_selections = params[:song][:genres]
-   @song.genre_id = genre_selections
-
-   @song.save
-
-   redirect to '/songs/#{@song.slug}'
-
- end
-
- get '/songs/:slug' do
-   @song = Song.find_by_slug(params[:slug])
-   erb :'songs/show'
- end
+ # post '/songs' do
+ #
+ #   artist_entry = params[:song][:artist]
+ #   if Artist.find_by(:name => artist_entry)
+ #     artist = Artist.find_by(:name => artist_entry)
+ #   else
+ #     @song = Song.create(:name => params[:song][:name])
+ #     artist = Artist.create(:name => artist_entry)
+ #   end
+ #   @song.artist = artist
+ #
+ #   genre_selections = params[:song][:genres]
+ #   @song.genre_id = genre_selections
+ #
+ #   @song.save
+ #
+ #   redirect to '/songs/#{@song.slug}'
+ #
+ # end
 
 
  patch '/songs/:slug' do #edit action
@@ -44,11 +44,6 @@ class SongsController < ApplicationController
     @song.save
 
     redirect '/songs/#{@song.slug}'
-  end
-
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :"songs/show"
   end
 
   get '/songs/:slug/edit' do
