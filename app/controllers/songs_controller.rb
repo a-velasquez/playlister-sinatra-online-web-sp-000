5ca32c992aa33800ce5ff6817b9f75e4ea92e270
Alex Velasquez <jtinnesco@gmail.com>
2020-03-29 20:57:54 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,6 +20,8 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
+
+    redirect :"/songs/#{@song.slug}"
   end
 
 
